cf4d9c6f603c6218a55423174508ad0554975fb0
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-20 18:23:08 -0800

    added files

--- src/app/itvsearch.ts
+++ src/app/itvsearch.ts
@@ -1,18 +1,12 @@
 export interface Itvsearch {
-    showSearch: array
+    showSearch: Array<object>
       show: object
-        showName: string
-        showStatus: string
-        showGenres: array
-        showSummary: string
-        showSchedule: object
-          scheduleTime: string
-          scheduleDays: array
-    cast: array
-      castPerson: object
-        personName: string
-    seasons: array
-        seasonsId: number
-        seasonsEpisodeOrder: number
+        name: string
+        status: string
+        genres: Array<string>
+        summary: string
+        schedule: object
+          time: string
+          days: Array<string>
 }
 
