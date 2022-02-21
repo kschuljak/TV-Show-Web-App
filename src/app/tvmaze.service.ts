6b686485d314647cc56b9bc44abcb432b1bac6f0
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-20 19:16:58 -0800

    update service

--- src/app/tvmaze.service.ts
+++ src/app/tvmaze.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TvmazeService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getShowInfo(name: string){
+    this.httpClient.get(`https://api.tvmaze.com/search/shows?q=${name}&appid=${environment.appId}`)
+  }
 }
