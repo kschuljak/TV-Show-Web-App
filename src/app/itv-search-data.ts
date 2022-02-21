84f3e653f0d95f96e830b816a9916c449a44c635
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-20 20:08:05 -0800

    update

---
+++ src/app/itv-search-data.ts
@@ -0,0 +1,15 @@
+export interface ITvSearchData {
+    show: {
+        name: string,
+        genres: Array<string>,
+        status: string,
+        schedule: {
+            time: string,
+            days: Array<string>
+        },
+        network: {
+            name: string
+        },
+        summary: string
+    }
+}
--- src/app/tvmaze.service.ts
+++ src/app/tvmaze.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { ITvSearchData } from './itv-search-data';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class TvmazeService {
   constructor(private httpClient: HttpClient) { }
 
   getShowInfo(name: string){
-    this.httpClient.get(`https://api.tvmaze.com/search/shows?q=${name}&appid=${environment.appId}`)
+    return this.httpClient.get<ITvSearchData>(`https://api.tvmaze.com/search/shows?q=${name}&appid=${environment.appId}`)
   }
 }
 
