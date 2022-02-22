9d114eb73eb95349e65dff5955f7d42c90a8ac20
jessica <jessica.vargas@headlight.com>
2022-02-21 16:32:33 -0800

    Jess files

---
+++ src/app/search-display/search-display.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchDisplayComponent } from './search-display.component';
+
+describe('SearchDisplayComponent', () => {
+  let component: SearchDisplayComponent;
+  let fixture: ComponentFixture<SearchDisplayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchDisplayComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/search-display/search-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-display',
+  templateUrl: './search-display.component.html',
+  styleUrls: ['./search-display.component.css']
+})
+export class SearchDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
