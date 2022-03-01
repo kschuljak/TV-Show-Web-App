64a1dd4001db56120b2b0093889ef1b67480d7d1
jessica <jessica.vargas@headlight.com>
2022-03-01 09:06:49 -0800

    Jess changes

--- src/app/show-search/show-search.component.ts
+++ src/app/show-search/show-search.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/dist/types/operators';
 
 @Component({
   selector: 'app-show-search',
@@ -7,12 +8,16 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./show-search.component.css']
 })
 export class ShowSearchComponent implements OnInit {
-  
-  search = new FormControl()
-
+  @Output() searchEvent = new EventEmitter<string>();
+  search = new FormControl('', [Validators.minLength(3)])
   constructor() { }
 
   ngOnInit(): void {
+    this.search.valueChanges
+    .pipe(debounceTime(1000))
+    .subscribe((searchValue: string) => {
+      if (!this.search.invalid){
+        this.searchEvent.emit(searchValue)
   }
 
 }
